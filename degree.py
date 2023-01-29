0afe0e650f5d9f840db893edccefbe6c4902ccae
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-01-29 09:31:56 +0200

    degree

---
+++ degree.py
@@ -0,0 +1,11 @@
+ahmed= 71
+if ahmed >= 90:
+    print('excellent')
+elif ahmed >=80:
+    print('very good')
+elif ahmed>=70:
+    print('good')
+elif ahmed>=60:
+    print('successfull')
+else:
+    print('not successfull')
