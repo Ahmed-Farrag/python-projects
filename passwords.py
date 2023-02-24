55d828812e882baeced08a595b01a854bb7dac19
Ahmed Farrag <47854268+Ahmed-Farrag@users.noreply.github.com>
2023-02-24 17:39:29 +0200

    generate passwords

---
+++ passwords.py
@@ -0,0 +1,8 @@
+import random as rand
+chars = "abcdefghijkimnopqrstuxvwyz"
+CHARS = chars.upper()
+symbols = "[]{}()*;/,._-"
+numbers = "0123456789"
+
+allVariables = chars + CHARS + symbols + numbers
+len = 16
